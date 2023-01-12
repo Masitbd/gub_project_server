60ce389e2b2c62aa07e4b2f5dbd001d7efa25c0f
sarker24 <sarker24@gmail.com>
2023-01-12 20:15:46 +0100

    Add online application form

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 const { MongoClient, ServerApiVersion } = require("mongodb");
+const { response } = require("express");
 
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(cors());
 app.use(express.json());
+
 
 // mondoDB connection
 
@@ -34,7 +37,9 @@ async function run() {
     await client.connect();
     const database = client.db("gub_portal");
     const teacherCollection = database.collection("teachers");
+    const applicationCollection = database.collection("onlineApplications");
 
+    // get api
     app.get("/teacher", async (req, res) => {
       const query = {};
 
@@ -42,9 +47,29 @@ async function run() {
       const teachers = await cursor.toArray();
       res.send(teachers);
     });
-  } finally {
+
+    // post api
+  app.post("/teacher", async (req, res) => {
+    const teacher = req.body;
+    //console.log('hello data', teacher)
+    const result = await teacherCollection.insertOne(teacher)
+    res.send(result)
+  });
+
+  app.post("/applyOnline", async (req, res) => {
+    const application = req.body;
+    //console.log('hello data', teacher)
+    const result = await applicationCollection.insertOne(application)
+    res.send(result)
+  });
+  
+  }
+  finally {
   }
 }
+
+  
+
 run().catch(console.dir);
 
 // mondoDB connection end
