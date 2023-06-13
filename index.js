00a359411b89f5f8f5e158446bf2cbedbdf8b9c8
sarker24 <sarker24@gmail.com>
2023-06-13 17:56:58 +0200

    Add pdf file

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const multer  = require('multer')
 const cors = require("cors");
 require("dotenv").config();
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const { response } = require("express");
+const mongoose = require('mongoose');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -10,6 +12,17 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const upload = multer({ dest: 'uploads/' }); 
+const fileSchema = new mongoose.Schema({
+  filename: String,
+  mimetype: String,
+  data: Buffer,
+  studentName: String,
+  studentId: String,
+  email: String,
+  research: String
+});
+const File = mongoose.model('File', fileSchema);
 
 // mondoDB connection
 
@@ -81,10 +94,29 @@ async function run() {
     res.send(result)
   });
 
-  app.post("/applyOnline", async (req, res) => {
-    const application = req.body;
-    //console.log('hello data', teacher)
-    const result = await applicationCollection.insertOne(application)
+  app.post("/applyOnline", upload.single('pdf'), async (req, res) => {
+    //const application = req.body;
+
+    const file = req.file;
+
+    /* const newFile = new File({
+      
+      //mimetype: file.mimetype,
+     // data: file.buffer,
+      studentName: req.body.studentName,
+      studentId: req.body.studentId,
+      email: req.body.email,
+      gender: req.body.gender,
+      research: req.body.research,
+      filename: file.filename,
+    }); */
+   //console.log(newFile);
+    const result = await applicationCollection.insertOne({ studentName: req.body.studentName,
+      studentId: req.body.studentId,
+      email: req.body.email,
+      gender: req.body.gender,
+      research: req.body.research,
+      filename: file.filename,})
     res.send(result)
   });
 
